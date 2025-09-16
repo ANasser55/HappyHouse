80331c00f93f35bbf8bacc21ebac318baac56ad1
Ahmed Nasser <A.Nasser36900@gmail.com>
2025-09-16 23:31:37 +0300

    Restructure project from Monolithic to Clean Architecture

--- HappyHouse.Client/obj/Debug/net8.0-windows/HappyHouse.Client.AssemblyInfo.cs
+++ HappyHouse.Client/obj/Debug/net8.0-windows/HappyHouse.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HappyHouse.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631912f51da537b72940b74cd4dd84753a8e19bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d96dca66a00c74e2c7e51e3d40d1ced91a968435")]
 [assembly: System.Reflection.AssemblyProductAttribute("HappyHouse.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HappyHouse.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
